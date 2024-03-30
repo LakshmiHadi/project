a10b3909fa64b8843beff3699de6460787b8a278
Lakshmi Hadi <149565944+LakshmiHadi@users.noreply.github.com>
2024-03-30 17:21:12 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Spotify");
+let songs = [
+    {songName: "salam-e-Ishq",filePath: "song/1.mp3",coverPath: "covers/1.jpg"},
+    {songName: "salam-e-Ishq",filePath: "song/1.mp3",coverPath: "covers/1.jpg"},
+    {songName: "salam-e-Ishq",filePath: "song/1.mp3",coverPath: "covers/1.jpg"},
+    {songName: "salam-e-Ishq",filePath: "song/1.mp3",coverPath: "covers/1.jpg"},
+    {songName: "salam-e-Ishq",filePath: "song/1.mp3",coverPath: "covers/1.jpg"},
+    {songName: "salam-e-Ishq",filePath: "song/1.mp3",coverPath: "covers/1.jpg"},
+]
+let audioElement = new Audio('1.mp3');
+// audioElement.play();
