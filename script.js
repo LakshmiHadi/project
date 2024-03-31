85033501442926a7a16d7cf48a419da606ed8e2e
Lakshmi Hadi <149565944+LakshmiHadi@users.noreply.github.com>
2024-03-31 15:37:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 console.log("Welcome to Spotify");
+
+//Initialize the variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myprogressBar');
+
 let songs = [
     {songName: "salam-e-Ishq",filePath: "song/1.mp3",coverPath: "covers/1.jpg"},
     {songName: "salam-e-Ishq",filePath: "song/1.mp3",coverPath: "covers/1.jpg"},
@@ -9,3 +16,15 @@ let songs = [
 ]
 let audioElement = new Audio('1.mp3');
 // audioElement.play();
+
+//Handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0{
+        audioElement.play();
+    })
+})
+//Listen to Events
+document.addEventListener('timeupdate',()=>{
+    console.log('timeupdate');
+    // update Seekbar
+})
