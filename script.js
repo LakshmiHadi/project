6ec5701fff22829691309265897f0b7d7b36e0cc
Lakshmi Hadi <149565944+LakshmiHadi@users.noreply.github.com>
2024-04-01 18:16:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,16 @@ let audioElement = new Audio('1.mp3');
 
 //Handle play/pause click
 masterPlay.addEventListener('click',()=>{
-    if(audioElement.paused || audioElement.currentTime<=0{
+    if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
-    })
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+    }
 })
 //Listen to Events
 document.addEventListener('timeupdate',()=>{
