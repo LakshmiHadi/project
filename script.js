4f1289aca206962ab5dbfc741a0b66e1e42679f9
Lakshmi Hadi <149565944+LakshmiHadi@users.noreply.github.com>
2024-04-08 18:05:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,11 +73,24 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     })
 })
 document.getElementById('next').addEventListener('click', ()=>{
-    if(songIndex>9){
+    if(songIndex>=9){
         songIndex = 0
     }
     else{
         songIndex +=1;
+    }
+    audioElement.src = 'songs/${songIndex+1}.mp3';
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -=1;
     }
     audioElement.src = 'songs/${songIndex+1}.mp3';
     audioElement.currentTime = 0;
